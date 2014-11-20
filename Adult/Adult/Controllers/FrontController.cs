0d7bef7136a74ba821d4e2c7cbccb828185efad9
unknown <burtonwu256@gmail.com>
2014-11-19 19:05:56 -0500

    Inital Commit

---
+++ Adult/Adult/Controllers/FrontController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adult.Controllers
+{
+    public class FrontController : Controller
+    {
+        //
+        // GET: /Front/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
