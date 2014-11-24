915ceab294ec7fd6a0b1dc90f5abbcbe101e6171
unknown <burtonwu256@gmail.com>
2014-11-24 14:34:39 -0500

    adding header

---
+++ Adult/assets/app/adultApp.js
@@ -0,0 +1,1 @@
+﻿angular.module('adultApp', ['head.directive', 'body.directive']);
---
+++ Adult/assets/app/head/directives/directives.js
@@ -0,0 +1,7 @@
+﻿angular.module('head.directive', [])
+    .directive('header', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/assets/app/head/templates/header.cshtml'
+        };
+    });
